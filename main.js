d6c219a7776c3f63903d9025390b429ef271d52d
Casimero Tanseco <tansecoc@oregonstate.edu>
2022-04-23 17:05:26 -0600

    created database connection config file and initial main.js file

---
+++ main.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var pgsql = require('./dbcon.js');
+var client = pgsql.client;
+var app = express();
+
+// Connect to Google Cloud SQL database
+client.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to database!");
+});
+
+// Print test query to console
+client.query(`SELECT * FROM users`, (err, res) => {
+    if (!err) {
+        console.log(res.rows);
+    } else {
+        console.log(err.message);
+    }
+    client.end()
+});
