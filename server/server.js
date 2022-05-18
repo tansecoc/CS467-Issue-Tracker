fffc94d708bfacfe83e67ce6d4a70d6c69b7023f
Kevin Peralta <peraltak@oregonstate.edu>
2022-05-18 15:07:30 -0700

    fixed invalid login logic and redirect issues

--- server/server.js
+++ server/server.js
@@ -3,12 +3,11 @@
 const process = require('process');
 const express = require('express');
 const session = require('express-session');
-// const pgSession = require('connect-pg-simple')(session);
 const knexSession = require('connect-session-knex')(session);
 const passport = require('passport');
 const pool = require('./config/database');
 require('./config/passport')
-// require('dotenv').config();                     // for injecting local environment
+//require('dotenv').config();                     // for injecting local environment
 const path = require('path');
 
 const app = express();
