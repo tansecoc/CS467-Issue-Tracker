526143c6038c35dffa9db02a3bbcb8c5b68469f8
Kevin Gilpin <kevin.gilpin@gmail.com>
2022-05-18 13:34:06 -0700

    Change users file name / route

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const knexSession = require('connect-session-knex')(session);
 const passport = require('passport');
 const pool = require('./config/database');
 require('./config/passport')
-//require('dotenv').config();                     // for injecting local environment
+// require('dotenv').config();                     // for injecting local environment
 const path = require('path');
 
 const app = express();
