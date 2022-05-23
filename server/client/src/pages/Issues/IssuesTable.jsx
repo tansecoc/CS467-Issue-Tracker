86289dd65d4910a02f7b8a96fcf51471e615db78
Kevin Gilpin <kevin.gilpin@gmail.com>
2022-05-22 23:43:43 -0700

    Rename sort function to sortBy

--- server/client/src/pages/Issues/IssuesTable.jsx
+++ server/client/src/pages/Issues/IssuesTable.jsx
@@ -4,19 +4,19 @@ import { useSort } from '../../utils/useSort';
 import { Issue } from './Issue';
 
 export function IssuesTable({ data }) {
-  const [issues, sort] = useSort(data);
+  const [issues, sortBy] = useSort(data);
 
   return (
     <TableContainer marginTop={10}>
       <Table variant='simple'>
         <Thead>
           <Tr>
-            <Th onClick={() => sort('type')} cursor="pointer">Type</Th>
-            <Th onClick={() => sort('priority')} cursor="pointer">Priority</Th>
-            <Th onClick={() => sort('status')} cursor="pointer">Status</Th>
-            <Th onClick={() => sort('title')} cursor="pointer">Title</Th>
-            <Th onClick={() => sort('dueDate')} cursor="pointer" isNumeric>Due Date</Th>
-            <Th onClick={() => sort('assignee')} cursor="pointer" isNumeric>Assignee</Th>
+            <Th onClick={() => sortBy('type')} cursor="pointer">Type</Th>
+            <Th onClick={() => sortBy('priority')} cursor="pointer">Priority</Th>
+            <Th onClick={() => sortBy('status')} cursor="pointer">Status</Th>
+            <Th onClick={() => sortBy('title')} cursor="pointer">Title</Th>
+            <Th onClick={() => sortBy('dueDate')} cursor="pointer" isNumeric>Due Date</Th>
+            <Th onClick={() => sortBy('assignee')} cursor="pointer" isNumeric>Assignee</Th>
           </Tr>
         </Thead>
         <Tbody>
