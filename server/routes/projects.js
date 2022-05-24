fe0db240e71906d1e274f28ebf105cfe19e49957
Casimero Tanseco <tansecoc@oregonstate.edu>
2022-05-23 19:27:23 -0700

    completed create projects endpoint

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,17 +1,37 @@
-const server = require('../server.js');
-const { Router } = require('express');
-const router = Router();
+const router = require("express").Router();
+const passport = require("passport");
+let pool = require("../config/database");
 
 /* ------------- Begin Model Functions ------------- */
 
-
+async function createOrg(pool, project_name, project_description, user_id, org_id) {
+    return await pool('projects').insert({
+        org_id: org_id,
+        project_creator_id: user_id,
+        project_name: project_name,
+        project_create_date: (new Date()).toISOString().split('T')[0],
+        project_description: project_description
+    });
+}
 
 /* ------------- End Model Functions ------------- */
 
 
 /* ------------- Begin Controller Functions ------------- */
 
-
+router.post('/', async (req, res) => {
+    try {
+        if (req.isAuthenticated()) {
+            await createOrg(pool, req.body.project_name, req.body.project_description, req.cookies.user_id, req.cookies.org_id);
+            res.send(true);
+        } else {
+            res.send('You are not authenticated');
+        } 
+    } catch (error) {
+        console.log(error);
+        res.send(false);
+    }
+})
 
 /* ------------- End Controller Functions ------------- */
 
