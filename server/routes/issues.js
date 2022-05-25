aad90feb46fa22fa3051815fdc5f0548afc4e947
Kevin Peralta <peraltak@oregonstate.edu>
2022-05-24 17:37:57 -0700

    finished issues endpoints

--- server/routes/issues.js
+++ server/routes/issues.js
@@ -14,8 +14,8 @@ async function getIssue(pool, issue_id){
     return issueData[0];
 }
 
-async function patchIssue(pool){
-    return await pool('issues').where().update({});
+async function patchIssue(pool, updateObject){
+    return await pool('issues').where().update(updateObject);
 }
 
 async function deleteIssue(pool, issue_id){
@@ -92,27 +92,50 @@ router.put('/:issue_id', async (req, res) => {
         if (req.isAuthenticated()) {
             getIssue(req.params.issue_id)
             .then(issue => {
+                // issue_id does not exist
                 if(issue === undefined || issue === null){
                     res.status(404).send(false).end();
                 } else{
-                    // Required
-                    let projectID = req.body.project_id;
-                    let issueCreatorID;
-                    if(req.cookies.user_id === undefined || req.cookies.user_id === null){
-                        res.status(400).send(false).end();
-                        return;
-                    } else{
-                        issueCreatorID = req.cookies.user_id;
-                    }
-                    let issueName = req.body.issue_name;
+                    // Sets JSON object and properties
+                    let issueUpdate = {};
+                    issueUpdate.issue_name = (req.body.issue_name !== undefined && req.body.issue_name !== null) ? req.body.issue_name : null;
+                    issueUpdate.issue_type = (req.body.issue_type !== undefined && req.body.issue_type !== null) ? req.body.issue_type : null;
+                    issueUpdate.issue_priority = (req.body.issue_priority !== undefined && req.body.issue_priority !== null) ? req.body.issue_priority : null;
+                    issueUpdate.issue_status = (req.body.issue_status !== undefined && req.body.issue_status !== null) ? req.body.issue_status : null;
+                    issueUpdate.issue_due_date = (req.body.issue_due_date !== undefined && req.body.issue_due_date !== null) ? req.body.issue_due_date : null;
+                    issueUpdate.issue_description = (req.body.issue_description !== undefined && req.body.issue_description !== null) ? req.body.issue_description : null;
 
-                    // Optional
-                    let issueType = (req.body.issue_type !== undefined && req.body.issue_type !== null) ? req.body.issue_type : null;
-                    let issuePriority = (req.body.issue_priority !== undefined && req.body.issue_priority !== null) ? req.body.issue_priority : null;
-                    let issueStatus = (req.body.issue_status !== undefined && req.body.issue_status !== null) ? req.body.issue_status : null;
-                    let issueDueDate = (req.body.issue_due_date !== undefined && req.body.issue_due_date !== null) ? req.body.issue_due_date : null;
-                    let issueDescription = (req.body.issue_description !== undefined && req.body.issue_description !== null) ? req.body.issue_description : null;
-                    let issueAssigneeEmail = (req.body.issue_assignee_email !== undefined && req.body.issue_assignee_email !== null) ? req.body.issue_assignee_email : null;
+                    // Removes any falsy values from update object
+                    for(let prop in issueUpdate){
+                        if(!(issueUpdate[prop])){
+                            delete issueUpdate.prop
+                        }
+                    }
+                    
+                    // Changing issue_assignee_id
+                    if(req.body.issue_assignee_email){
+                        // Checks if issue_assignee_email is valid
+                        getUserInfo(pool, req.body.issue_assignee_email)
+                        .then(user => {
+                            // User with issue_assignee_email found
+                            if(user !== undefined || user !== null){
+                                issueUpdate.issue_assignee_id = user.user_id
+                                patchIssue(pool, issueUpdate)
+                                .then(result => {
+                                    res.status(200).send(true).end()
+                                })
+                            // No user with issue_assignee_email found
+                            } else{
+                                res.status(404).send(false).end()
+                            }
+                        })
+                    // Not changing issue_assignee_id
+                    } else{
+                        patchIssue(pool, issueUpdate)
+                        .then(result => {
+                            res.status(200).send(true).end()
+                        })
+                    }
                 }
             })
         } else{
