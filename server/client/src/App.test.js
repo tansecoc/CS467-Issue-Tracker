29571f844980e3779374c725630303f62d343fc5
Kevin Gilpin <kevin.gilpin@gmail.com>
2022-06-12 00:31:02 -0700

    Test App routing with Sign In

--- server/client/src/App.test.js
+++ server/client/src/App.test.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import { render } from './test-utils';
-import App from './App';
+import { render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {createMemoryHistory} from 'history'
+import {BrowserRouter} from 'react-router-dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn chakra/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import '@testing-library/jest-dom'
+
+import App from './app'
+
+window.matchMedia = window.matchMedia || function() {
+  return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {}
+  };
+};
+
+test('full app rendering/navigating', async () => {
+  const history = createMemoryHistory()
+  render(
+    <BrowserRouter location={history.location} navigator={history}>
+      <App />
+    </BrowserRouter>,
+  )
+  const user = userEvent.setup()
+  // verify page content for expected route
+  // often you'd use a data-testid or role query, but this is also possible
+  expect(screen.getByText(/Boost your Productivity/i)).toBeInTheDocument()
+
+  await user.click(screen.getByText(/Sign In/i))
+
+  // check that the content changed to the new page
+  expect(screen.getByText(/Sign in to your account/i)).toBeInTheDocument()
+})
